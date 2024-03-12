cdd655ffc1f9d3011996295779f82f7a723aac8b
JuanUruloki <juampy_1@live.com>
2024-03-12 17:37:16 -0300

    logo changes

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 
-
 const Hero = () => {
   return (
     <>
       <section
         id="home"
-        className="dark:bg-gray-dark relative z-10 overflow-hidden bg-white pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+        className="relative z-10 overflow-hidden bg-white pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
       >
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
@@ -17,22 +16,20 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <Image
-                  src="/images/logo/logo-2.png"
+                  src="/images/logo/logo-3.png"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.png"
+                  src="/images/logo/logo-3.png"
                   alt="logo"
                   width={140}
                   height={30}
                   className="hidden w-full dark:block"
                 />
-                <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-body-color sm:text-lg md:text-xl">
-                  
-                </p>
+                <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl"></p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   {/* <Link
                     href="https://nextjstemplates.com/templates/saas-starter-startup"
