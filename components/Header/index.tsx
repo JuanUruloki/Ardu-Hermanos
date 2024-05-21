3bde34202c1263cc36ec22d5958b69cd47ac0fd5
JuanUruloki <juampy_1@live.com>
2024-05-21 15:39:22 -0300

    fix_adjusting carousel_adding logo on navBar

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -48,7 +48,7 @@ const Header = () => {
         }`}
       >
         <div className="container">
-          <div className="relative -mx-8 flex items-center justify-between">
+          <div className={`relative -mx-8 mt-12 flex items-center justify-between ${!sticky ? "bg-gray-200 dark:bg-gray-700": "bg-transparent pb-12"}`}>
             <div className="flex w-full items-center justify-between px-4">
               <div>
                 <button
@@ -136,24 +136,12 @@ const Header = () => {
                 </nav>
               </div>
               <div className="flex items-center justify-end pr-16 lg:pr-0">
-                {/* <Link
-                  href="/signin"
-                  className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
-                >
-                  Sign In
-                </Link>
-                <Link
-                  href="/signup"
-                  className="ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-sm bg-primary px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9"
-                >
-                  Sign Up
-                </Link> */}
                 <div>
                   <ThemeToggler />
                 </div>
               </div>
             </div>
-            <div className="hidden lg:block">
+            <div className="hidden lg:block mr-4 ">
 
             <Image
             src="/images/logo/logo_dark.svg"
