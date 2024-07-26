e37a5b61d1ec124e77794f2764e47c1ffad5856c
JuanUruloki <juampy_1@live.com>
2024-07-25 23:44:00 -0300

    fix_AboutPage redesign

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
-import Breadcrumb from "@/components/Common/Breadcrumb";
+import Contact from "@/components/Contact";
 
 import { Metadata } from "next";
 
@@ -12,11 +12,31 @@ export const metadata: Metadata = {
 const AboutPage = () => {
   return (
     <>
-      <Breadcrumb
-        pageName="Sobre nosotros"
-        description="Cultura institucional"
-      />
+    <div className="relative z-10 overflow-hidden h-[80vh] pb-16 pt-[200px] bg-light dark:bg-blueArdu md:pb-[0px] md:pt-[200px] xl:pb-[0px] xl:pt-[200px] 2xl:pb-[0px] 2xl:pt-[200px]">
+
+    <div className=" z-10 h-[919px]  w-full overflow-hidden bg-cover bg-center bg-no-repeat">
+        <div className="absolute inset-0 h-full w-full overflow-hidden">
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="absolute left-0 top-0 hidden h-full w-full object-cover md:block"
+            src="/videos/Planta.mp4"
+          />
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="absolute left-0 top-0 block h-full w-full object-cover md:hidden"
+            src="/videos/PlantaMobile.mp4"
+          />
+        </div>
+      </div>
+    </div>
       <AboutSectionOne />
+      <Contact/>
     </>
   );
 };
